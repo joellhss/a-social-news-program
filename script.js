8186f5a3d6ee19be79569c1d3486d8d366ec2a27
Joel Sousa <joelsousa@pop-os.localdomain>
2022-08-05 11:56:13 -0300

    challenger 2

--- script.js
+++ script.js
@@ -1,68 +1,126 @@
-const listLink = [];
-start()
+const listLink = [
+    {
+        title: "Hacker News",
+        URL: "https://news.ycombinator.com",
+        author: "Baptiste"
+    },
+    {
+        title: "Reddit",
+        URL: "https://reddit.com",
+        author: "Thomas"
+    },
+    {
+        title: "Boing Boing",
+        URL: "https://boingboing.net",
+        author: "Daniel"
+    },
+];
 
-function start() {
-    const insertUser = prompt("Choose an option: \n1: Show links \n2: Add a link \n3: Remove a link \n0: Quit");
-    const insertUserLowcase = insertUser.toLowerCase()
 
-    verifyOption(insertUserLowcase)
 
-}
+const submit = document.getElementById('submit')
 
-function verifyOption(x) {
-    switch (x) {
-        case "1":
-        case "show links":
-            showLinks();
-            break;
-        case "2":
-        case "add a link":
-            addLinks();
-            break;
-        case "3":
-        case "remove a link":
-            removeLinks();
-            break;
-        case "0":
-        case "quit":
-            break;
-        case "4":
-        case "menu":
-            start()
-            break;
-        default:
-            verifyOption(prompt("I didn't understand! \nChoose an option: \n1: Show links \n2: Add a link \n3: Remove a link \n0: Quit").toLowerCase())
-    }
-}
+showLinks()
 
-function addLinks() {
+submit.addEventListener("click", e => {
+    const form = document.createElement('form')
+    form.id = "form"
+    form.innerHTML = `<fieldset>
+            <label for="title-link">
+                <p>Title:</p>
+                <input type="text" id="title-link" placeholder="Enter title here...">
+            </label>
+            
+            <label for="author-link">
+                <p>Author:</p>
+                <input type="text" id="author-link" placeholder="Enter author name here...">
+            </label>
+            
+            <label for="url-link">
+                <p>URL:</p>
+                <input type="url" id="url-link" placeholder="www.example.com">
+            </label>
+            <button type="submit" id="submit-form">Add link</button>
+    </fieldset>`
+
+    const body = document.querySelectorAll('body')
+    const main = document.querySelector('main')
+    const bodyArray = Array(body[0].children)
+
+    bodyArray.forEach(e => {
+        if (e.form === undefined) {
+            body[0].insertBefore(form, main)
+            document.querySelector("header").style.marginBottom = "15px";
+        }
+    })
+
+    const buttonAdd = document.getElementById('submit-form')
+
+
+    buttonAdd.addEventListener("click", e => {
+    e.preventDefault()
+    
     let receiveLink = {
         title: "",
         URL: "",
         author: ""
     }
     
-    receiveLink.title = prompt("Enter your link title.")
-    receiveLink.URL = prompt("Enter your link URL.")
-    receiveLink.author = prompt("Enter the author of your link.")
+    receiveLink.title = e.target.form[1].value;
+    receiveLink.URL = e.target.form[3].value;
+    receiveLink.author = e.target.form[2].value;
 
     if (receiveLink.URL.startsWith("http://" || "https://") === false) {
         let sumText = "https://" + receiveLink.URL
         receiveLink.URL = sumText
     }
 
+
     listLink.push(receiveLink)
-    start()
-}
+    showLinks()
+
+    document.getElementById('title-link').value=""
+    document.getElementById('author-link').value=""
+    document.getElementById('url-link').value=""
+
+    document.querySelector('body').removeChild(document.querySelector('form'))
+
+        const thanks = document.createElement('p')
+        thanks.id = "thanks"
+        thanks.innerText = "Thanks for collaborating!"
+        body[0].insertBefore(thanks, main)
+
+        setTimeout(() => {
+            document.querySelector('body').removeChild(document.getElementById('thanks'))
+            document.querySelector("header").style.marginBottom = "44px";
+        }, 2000)
+
+
+})
+
+
+})
+
 
 function showLinks() {
+    const main = document.querySelector('main')
+
     let sumListLinks = ""
+
     listLink.forEach(element => {
-        sumListLinks += `\n ${listLink.indexOf(element) + 1} - Title: ${element.title} | URL: ${element.URL} | Author: ${element.author}`
+        sumListLinks += `<div id="content">
+        <div id="header">
+            <a href="${element.URL}" id="title">${element.title}</a>
+            <a href="${element.URL}" id="url">${element.URL}</a>
+        </div>
+        <p>Submited by <span id="author">${element.author}</span></p>
+    </div>`
+    
+    main.innerHTML = sumListLinks 
+
     })
 
     
-    verifyOption(prompt(`Links: ${sumListLinks} \n\n\nChoose an option: \n2: Add a link | 3: Remove a link | 4: menu`))
 }
 
 function removeLinks() {
@@ -82,5 +140,3 @@ function removeLinks() {
     }
     
 }
-
-console.log(listLink)
